648c05c9c79db0e365598259a84918499c7ba703
Alexandru Dinu <dinualexandru9@yahoo.com>
2024-09-13 15:05:11 +0300

    Update main.cpp

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -5,23 +5,23 @@
 #include <tchar.h>
 #include <thread>
 
-#pragma comment(lib, "ws2_32.lib") //ofera functii pentru a crea, lega, asculta, accepta, trimite si primi date prin socket-uri de retea
+#pragma comment(lib, "ws2_32.lib") 
 
 
-bool initialize() //initializam biblioteca "Winsock"
+bool initialize() 
 {
-    WSADATA data; //structura "WSADATA" contine informatii despre implementarea Windows Sockets
-    return WSAStartup(MAKEWORD(2, 2), &data) == 0; //MAKEWORD creeaza o valoare de 16 biti din 2 valori de 8 biti (solicitand versiunea 2.2 a Winsock)
+    WSADATA data; 
+    return WSAStartup(MAKEWORD(2, 2), &data) == 0; 
 }
 
-void InteractWithClient(SOCKET clientSocket, std::vector<SOCKET>& clients) //facilitam comunicarea dintre server si client, gestionand primirea si difuzarea mesajelor catre alti clienti 
+void InteractWithClient(SOCKET clientSocket, std::vector<SOCKET> &clients) 
 {
     std::cout << "Client connected" << std::endl;
-    char buffer[4096]; //stocam datele primite de la client
+    char buffer[4096]; 
 
-    while (1) //intram intr-o bucla infinita pentru a primi si procesa continuu date de la client
+    while (1) 
     {
-        int bytesreceived = recv(clientSocket, buffer, sizeof(buffer), 0); //primeste date de la socket-ul clientului; "recv" returneaza numarul de octeti primiti
+        int bytesreceived = recv(clientSocket, buffer, sizeof(buffer), 0); 
 
         if (bytesreceived <= 0)
         {
@@ -29,40 +29,40 @@ void InteractWithClient(SOCKET clientSocket, std::vector<SOCKET>& clients) //fac
             break;
         }
 
-        std::string message(buffer, bytesreceived); //construim un string din buffer-ul primit, folosind numarul de octeti primiti 
+        std::string message(buffer, bytesreceived); 
         std::cout << "Message from client: " << message << std::endl;
 
-        for (auto client : clients) //iteram peste toti clientii conectati
+        for (auto client : clients) 
         {
-            if (client != clientSocket) //ne asiguram ca mesajul nu este trimis inapoi expeditorului initial
+            if (client != clientSocket) 
             {
-                send(client, message.c_str(), message.length(), 0); //trimitem mesajul altor clienti
+                send(client, message.c_str(), message.length(), 0); 
             }
         }
     }
-    auto it = find(clients.begin(), clients.end(), clientSocket); //cautam pozitia "clientSocket" in vectorul clienti
+    auto it = find(clients.begin(), clients.end(), clientSocket); 
 
-    if (it != clients.end()) //verificam daca socket-ul clientului a fost gasit in lista
+    if (it != clients.end()) 
     {
-        clients.erase(it); //daca este gasit, il eliminam din vectorul clienti
+        clients.erase(it); 
     }
 
-    closesocket(clientSocket); //inchidem socket-ul asociat cu clientul pentru a curata resurse
+    closesocket(clientSocket); 
 }
 
 
 int main()
 {
-    if (!initialize()) //apelam functia de initializare pentru a initializa Winsock
+    if (!initialize()) 
     {
         std::cout << "Winsock initialization failed" << std::endl;
-        return 1; //daca initializarea esueaza, este afisat un mesaj de eroare si returneaza 1, indicand o eroare
+        return 1; 
     };
 
     std::cout << "Server program" << std::endl;
 
 
-    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0); //cream un socket cu AF_INET (IPv4), SOCK_STREAM (TCP) si 0 (protocol implicit)
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0); 
 
     if (listenSocket == INVALID_SOCKET)
     {
@@ -70,13 +70,13 @@ int main()
         return 1;
     }
 
-    //configuram adresa serverului
+    
     int port = 12345;
-    sockaddr_in serveraddr; //declaram o variabila numita "serveraddr" de tip "sockaddr_in"; "sockaddr_in" este o structura definita in "winsock2.h" utilizata pentru adresele IPv4
-    serveraddr.sin_family = AF_INET; //seteaza membrul "sin_family" al structurii "serveraddr" la AF_INET, care este o constanta ce specifica familia de adrese pentru adresele IPv4
-    serveraddr.sin_port = htons(port); //seteaza numarul portului pentru socket-ul serverului, transformandu-l din ordinea octetilor gazdei, in ordinea octetilor de retea
+    sockaddr_in serveraddr; 
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_port = htons(port); 
 
-    if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1) //convertim adresa ip (0.0.0.0) in format binar; daca setarea adresei esueaza..
+    if (InetPton(AF_INET, _T("0.0.0.0"), &serveraddr.sin_addr) != 1) 
     {
         std::cout << "Setting address structure failed" << std::endl;
         closesocket(listenSocket);
@@ -84,7 +84,7 @@ int main()
         return 1;
     }
 
-    if (bind(listenSocket, (sockaddr*)&serveraddr, sizeof(serveraddr)) == SOCKET_ERROR) //legam socket-ul la adresa serverului
+    if (bind(listenSocket, (sockaddr*)&serveraddr, sizeof(serveraddr)) == SOCKET_ERROR) 
     {
         std::cout << "Bind failed" << std::endl;
         closesocket(listenSocket);
@@ -92,7 +92,7 @@ int main()
         return 1;
     }
 
-    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) //setam socket-ul sa asculte solicitarile de conectare primite; SOMAXCONN specifica lungimea maxima pentru coada de conexiuni in asteptare 
+    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) 
     {
         std::cout << "Listen failed" << std::endl;
         closesocket(listenSocket);
@@ -105,7 +105,7 @@ int main()
 
     std::vector<SOCKET> clients;
 
-    while (1) //intram intr-o bucla infinita pentru a accepta (continuu) noi conexiuni client
+    while (1) 
     {
         SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
 
@@ -114,13 +114,13 @@ int main()
             std::cout << "Invalid client socket" << std::endl;
         }
 
-        clients.push_back(clientSocket); //adaugam un nou client socket la vectorul clienti
-        std::thread t1(InteractWithClient, clientSocket, std::ref(clients)); //cream un nou thread pentru a gestiona comunicarea cu clientul
-        t1.detach(); //detasam thread-ul pentru a-i permite sa ruleze independent
+        clients.push_back(clientSocket); 
+        std::thread t1(InteractWithClient, clientSocket, std::ref(clients)); 
+        t1.detach(); 
     }
 
-    closesocket(listenSocket);
-    WSACleanup();
+    closesocket(listenSocket); 
+    WSACleanup(); 
 
     return 0;
 }
